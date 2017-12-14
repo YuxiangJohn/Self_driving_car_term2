59978539b4f7e2fd8b8eecb2a0506d8ae778ef94
YuxiangJohn <893274008@qq.com>
2017-12-14 00:00:34 -0500

    update

---
+++ PID_Control_Project/src/PID.cpp
@@ -0,0 +1,36 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_init, double Ki_init, double Kd_init) {
+	Kp = Kp_init;
+	Ki = Ki_init;
+	Kd = Kd_init;
+	
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
+
+}
+
+void PID::UpdateError(double cte) {
+	
+	i_error += cte;
+	d_error = cte - p_error;
+	p_error = cte;
+	
+}
+
+double PID::TotalError() {
+
+	return -Kp * p_error - Ki * i_error - Kd * d_error;
+}
+
